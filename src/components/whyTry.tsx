5d11330eb8ede5b348c33cc2889591924801a395
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-11 22:47:52 -0200

    Create whyTry.tsx

---
+++ src/components/whyTry.tsx
@@ -0,0 +1,10 @@
+export function WhyTry(){
+  return(
+    <div className="text-center mt-5">
+      <h2>
+        Why Should You <span className="text-danger">Try?</span>
+      </h2>
+      <img src="https://pngimg.com/uploads/burger_sandwich/small/burger_sandwich_PNG96775.png" alt="Hamburger" className="w-25"/>
+    </div>
+  )
+}
