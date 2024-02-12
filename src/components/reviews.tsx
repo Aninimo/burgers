2bdc4c6d525c292168ac3c5ee0da9a2cc75c1807
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-11 22:49:44 -0200

    Create reviews.tsx

---
+++ src/components/reviews.tsx
@@ -0,0 +1,51 @@
+export function Reviews(){
+  return(
+    <div className="text-center mt-5 mb-4">
+      <h2>
+        Customer <span className="text-danger">Reviews</span>
+      </h2>
+      <div className="d-flex justify-content-center p-4 mt-4 reviews">
+        <div className="d-flex">
+          <img
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VXNlcnxlbnwwfHwwfHx8MA%3D%3D"
+            alt="user image"
+            className="imgProfile rounded-circle"
+         />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vivamus pellentesque convallis felis. Nunc semper fermentum
+              mauris, vitae cursus nibh pulvinar ac. Sed facilisis purus eget
+              nisi porttitor mollis sed laoreet sapien.
+          </p>
+        </div>
+
+        <div className="d-flex">
+          <img
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VXNlcnxlbnwwfHwwfHx8MA%3D%3D"
+            alt="user image"
+            className="imgProfile rounded-circle"
+         />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vivamus pellentesque convallis felis. Nunc semper fermentum
+              mauris, vitae cursus nibh pulvinar ac. Sed facilisis purus eget
+              nisi porttitor mollis sed laoreet sapien.
+          </p>
+        </div>
+        <div className="d-flex">
+          <img
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8VXNlcnxlbnwwfHwwfHx8MA%3D%3D"
+            alt="user image"
+            className="imgProfile rounded-circle"
+         />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vivamus pellentesque convallis felis. Nunc semper fermentum
+              mauris, vitae cursus nibh pulvinar ac. Sed facilisis purus eget
+              nisi porttitor mollis sed laoreet sapien.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
