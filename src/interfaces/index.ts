c4ac764d7e03e472f0f53209de64a199a8dbc9c3
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2024-02-11 23:23:25 -0200

    Create index.ts

---
+++ src/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface CartProps{
+  id: string;
+  name: string;
+  imageURL: string;
+  price: number;
+}
+
+export interface ProductProps{
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  imageURL: string;
+}
+
+export interface CardCartProps{
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
